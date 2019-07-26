4d6b10f86214aefd072fce1a755932e1ac7ef49a
olaneat <olaneat@yahoo.com>
2019-07-26 12:20:40 +0100

    add to cart modification

--- cart/views.py
+++ cart/views.py
@@ -1,28 +1,35 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
-from .carts import Cart
+from .cart import Cart
 from .forms import CartForm
 from shop.models import Product
 
 @require_POST
 def add_cart(request, product_id, slug):
-    cart = Cart(request)
+    add_item = Cart(request)
     product = get_object_or_404(Product, id=product_id, slug = slug)
-    form = CartForm(request.POST)
-    if form.is_valid():
-        cd = form.cleaned_data
-        cart.add(product=product, 
-                        quantity = cd['quantity'],
-                        update_quantity = cd['update'])
-        return redirect('cart:cart:detail')
+    cart_form = CartForm(request.POST)
+    if cart_form.is_valid():
+        cd = cart_form.cleaned_data
+        add_item.add(product=product,
+                 quantity=cd['quantity'],
+                 update_quantity=cd['update'])
+    return redirect('cart:cart_detail')
+
+
 
 def remove_cart(request, product_id):
-        cart = Cart(request)
-        product = get_object_or_404(Product, product_id)
-        cart.remove(product)
-        return redirect('cart: cart_detail')
+        remove_item = Cart(request)
+        product = get_object_or_404(Product, id =product_id)
+        remove_item.remove(product)
+        return redirect('cart:cart_detail')
 
 def cart_detail(request):
         cart = Cart(request)
-        return render(request, 'cart/cart-detail.html', {'cart': cart})
+        for item in cart:
+                item['update_quantity'] = CartForm(
+                        initial={'quantity': item['quantity'],
+                        'update': True}
+                )
+        return render(request, 'cart/cart_detail.html', {'cart': cart})
 
